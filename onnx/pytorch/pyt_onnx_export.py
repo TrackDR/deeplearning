57b0c7c2ef37b44a151913b88bb5a6a8742173a5
TrackDR <jeremydjackson@gmail.com>
2021-11-23 21:24:52 -0500

    Create pyt_onnx_export.py

---
+++ onnx/pytorch/pyt_onnx_export.py
@@ -0,0 +1,24 @@
+# https://deci.ai/resources/blog/how-to-convert-a-pytorch-model-to-onnx/
+
+import torch
+import torchvision.models as models
+model = models.resnet50(pretrained=True)
+
+# ensure model is in inference mode
+model.eval()
+
+# Next, we create that dummy input variable.
+dummy_input = torch.randn(1, 3, 224, 224)
+
+# Let’s also define the input and output names.
+input_names = [ "actual_input" ]
+output_names = [ "output" ]
+
+torch.onnx.export(model, 
+                  dummy_input,
+                  "resnet50.onnx",
+                  verbose=False,
+                  input_names=input_names,
+                  output_names=output_names,
+                  export_params=True,
+                  )
