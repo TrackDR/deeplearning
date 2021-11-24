8cb06c538884819bcf304ffae52cba71b41d4d04
TrackDR <jeremydjackson@gmail.com>
2021-11-23 21:26:48 -0500

    Create pyt_onnx_import.py

---
+++ onnx/pytorch/pyt_onnx_import.py
@@ -0,0 +1,13 @@
+# https://deci.ai/resources/blog/how-to-convert-a-pytorch-model-to-onnx/
+# pip install onnxruntime
+
+import onnxruntime as onnxrt
+import torch
+
+onnx_session= onnxrt.InferenceSession("resnet50.onnx")
+
+img = torch.randn(1, 3, 224, 224)
+input_name = onnx_session.get_inputs()[0].name
+onnx_inputs = {input_name:img.numpy()}
+onnx_output = onnx_session.run(None, onnx_inputs)
+img_label = onnx_output[0]
